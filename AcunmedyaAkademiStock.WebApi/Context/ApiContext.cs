edf8a101ec62005f9a036e00d166cdc42f83c7c3
hamzaozalp <hamzaozalp@yaani.com>
2025-04-21 08:39:37 +0300

    Add project files.

---
+++ AcunmedyaAkademiStock.WebApi/Context/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using AcunmedyaAkademiStock.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunmedyaAkademiStock.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HAMZA;initial catalog=AcunmedyaApiDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ AcunmedyaAkademiStock.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using AcunmedyaAkademiStock.WebApi.Context;
+using AcunmedyaAkademiStock.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcunmedyaAkademiStock.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var value = _context.Categories.ToList();
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Ekleme başarılı");
+        }
+        [HttpDelete]
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı");
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);   
+            _context.SaveChanges();
+            return Ok("Güncelleme başarılı");
+        }
+
+    }
+}
